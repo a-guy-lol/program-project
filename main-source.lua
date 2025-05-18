539a04852b5cf9f1fd8a8e7c070e2d61ea993e09
A guy lol <meisguy.x9@gmail.com>
2025-05-18 18:39:17 -0400

    Update main-source.lua

--- main-source.lua
+++ main-source.lua
@@ -1,8 +1,1 @@
-local UserInputService = game:GetService("UserInputService")
-loadstring(game:HttpGet('https://pastebin.com/raw/HYuVwnZK'))()
-local function isMobile()
-    return UserInputService.TouchEnabled and not UserInputService.MouseEnabled
-end
-if not isMobile() then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/a-guy-lol/program-project/refs/heads/main/zexonMain/zexon-source.lua'))()
-end
+game.Players.LocalPlayer:Kick("You cannot use this service.")
